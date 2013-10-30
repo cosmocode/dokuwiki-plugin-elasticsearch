eff04e81d253de998aa69cc410eea99100ee66ab
rkaldung <roy@kaldung.com>
2013-10-30 13:39:15 +0100

    output namespace

--- src/elasticsearch/action/indexing.php
+++ src/elasticsearch/action/indexing.php
@@ -69,8 +69,8 @@ class action_plugin_elasticsearch_indexing extends DokuWiki_Action_Plugin {
             if ($this->needs_indexing($ID)) {
                 $this->index_page($ID);
             }
-            $acl = auth_loadACL();
             $this->log($AUTH_ACL);
+            $this->log(getNS($id));
         }
     }
 
