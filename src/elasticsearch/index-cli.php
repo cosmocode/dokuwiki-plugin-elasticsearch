443a1fe9d782448c5cec4b7b924de32e3e0741c7
rkaldung <roy@kaldung.com>
2013-10-30 12:05:32 +0100

    added cli command file

---
+++ src/elasticsearch/index-cli.php
@@ -0,0 +1,22 @@
+<?php
+
+$constants = array( 'DOKU_INC', 'DOKU_PLUGIN', 'DOKU_CONF', 'DOKU_E_LEVEL',
+    'DOKU_REL', 'DOKU_URL', 'DOKU_BASE', 'DOKU_BASE', 'DOKU_LF', 'DOKU_TAB',
+    'DOKU_COOKIE', 'DOKU_SCRIPT', 'DOKU_TPL', 'DOKU_TPLINC'
+);
+foreach($constants as $const) {
+    if(!defined($const)) {
+        $env_var = getenv($const);
+        if($env_var !== false) {
+            define($const, $env_var);
+        }
+    }
+}
+$ini_path = defined('DOKU_INC') ? DOKU_INC : realpath(dirname(__FILE__).'/../../../').'/';
+
+require_once($ini_path.'inc/init.php');
+require_once(DOKU_INC.'inc/common.php');
+require_once(DOKU_INC.'inc/search.php');
+require_once(DOKU_INC.'inc/pageutils.php');
+require_once DOKU_INC.'inc/cliopts.php';
+//require_once(dirname(__FILE__).'/action/indexing.php');
