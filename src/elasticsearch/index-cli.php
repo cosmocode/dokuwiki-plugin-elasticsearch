ea4a33b58078bf838c21a2e1834a0245c725e1cb
rkaldung <roy@kaldung.com>
2014-02-25 14:24:25 +0100

    minor change

--- src/elasticsearch/index-cli.php
+++ src/elasticsearch/index-cli.php
@@ -25,7 +25,7 @@ require_once(dirname(__FILE__).'/action/indexing.php');
 function index_all_pages(){
     global $conf, $ID;
     $data = array();
-    search($data,$conf['datadir'], 'search_allpages', array());
+    search($data,$conf['datadir'], 'search_allpages', array('skipacl'));
 
     foreach($data as $val) {
         $ID = $val['id'];
