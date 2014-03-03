0113dc2edb0bf2d49e8624ff2f86175405780816
rkaldung <roy@kaldung.com>
2014-03-03 14:58:49 +0100

    fxied bug

--- src/elasticsearch/action/indexing.php
+++ src/elasticsearch/action/indexing.php
@@ -152,7 +152,7 @@ class action_plugin_elasticsearch_indexing extends DokuWiki_Action_Plugin {
         $mainnamespace .= ':start';
         $metadata_ns = p_get_metadata($mainnamespace, '', true);
         $data['namespace'] = $metadata_ns['title'];
-        $data['namespace'] = str_repeat('*', '', $data['namespace']);
+        $data['namespace'] = str_replace('*', '', $data['namespace']);
 
         $data['groups']    = $this->getPageACL($id);
 
