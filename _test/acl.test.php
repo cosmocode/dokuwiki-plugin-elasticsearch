20652e344b3f3360aee33e0d905223202cb6f1ec
Andreas Gohr <gohr@cosmocode.de>
2019-07-03 13:56:36 +0200

    make ACL tests use a data provider
    This makes it easier to add additional cases

--- _test/acl.test.php
+++ _test/acl.test.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Helper tests for the elasticsearch plugin
  *
@@ -9,64 +10,73 @@ class helper_plugin_elasticsearch_test extends DokuWikiTest
 {
     protected $pluginsEnabled = ['elasticsearch'];
 
-    /**
-     * Parsed ACLs for a given page
-     */
-    public function test_acl()
-    {
-        $acl = [
-            '*	@user	8',
-            '*	@ALL	0',
-            'macro:*	@user	0',
-            'macro:*	@mino	8',
-            'mini:micro:nano:stop	@ALL	0',
-            'mini:*	@user	0',
-            'mini:micro:*	mirco	0',
-            'mini:micro:*	@user	8',
-            'mini:micro:nano:*	@micra	8',
-            'mini:micro:nano:*	@nana	8',
-            'mini:micro:nano:stop	spots	8',
-            'mini:micro:nano:*	@ALL	0',
-        ];
+    protected $acl = [
+        '*	@user	8',
+        '*	@ALL	0',
+        'macro:*	@user	0',
+        'macro:*	@mino	8',
+        'mini:micro:nano:stop	@ALL	0',
+        'mini:*	@user	0',
+        'mini:micro:*	mirco	0',
+        'mini:micro:*	@user	8',
+        'mini:micro:nano:*	@micra	8',
+        'mini:micro:nano:*	@nana	8',
+        'mini:micro:nano:stop	spots	8',
+        'mini:micro:nano:*	@ALL	0',
+    ];
 
+    /**
+     * Provide the test data
+     *
+     * @return array
+     */
+    public function data()
+    {
+        return [
+            [
+                'mini:micro:nano:stop',
+                [
+                    'groups_include' => [],
+                    'groups_exclude' => ['ALL'],
+                    'users_include' => ['spots'],
+                    'users_exclude' => [],
+                ]
+            ],
+            [
+                'mini:micro:nano:start',
+                [
+                    'groups_include' => ['micra', 'nana'],
+                    'groups_exclude' => ['ALL'],
+                    'users_include' => [],
+                    'users_exclude' => [],
+                ]
+            ],
+            [
+                'mini:micro:start',
+                [
+                    'groups_include' => ['user'],
+                    'groups_exclude' => ['ALL'],
+                    'users_include' => [],
+                    'users_exclude' => ['mirco'],
+                ]
+            ],
+        ];
+    }
+
+
+    /**
+     * Check parsed ACLs for a given page
+     *
+     * @dataProvider data
+     */
+    public function test_acl($page, $expected)
+    {
         /** @var helper_plugin_elasticsearch_acl $helper */
         $helper = plugin_load('helper', 'elasticsearch_acl');
 
-        $expected = [
-            'groups_include' => [],
-            'groups_exclude' => ['ALL'],
-            'users_include' => ['spots'],
-            'users_exclude' => [],
-        ];
-
-        $raw = $helper->getPageACL('mini:micro:nano:stop', $acl);
+        $raw = $helper->getPageACL($page, $this->acl);
         $actual = $helper->splitRules($raw);
 
         $this->assertEquals($expected, $actual);
-
-        $expected = [
-            'groups_include' => ['micra', 'nana'],
-            'groups_exclude' => ['ALL'],
-            'users_include' => [],
-            'users_exclude' => [],
-        ];
-
-        $raw = $helper->getPageACL('mini:micro:nano:start', $acl);
-        $actual = $helper->splitRules($raw);
-
-        $this->assertEquals($expected, $actual);
-
-        $expected = [
-            'groups_include' => ['user'],
-            'groups_exclude' => ['ALL'],
-            'users_include' => [],
-            'users_exclude' => ['mirco'],
-        ];
-
-        $raw = $helper->getPageACL('mini:micro:start', $acl);
-        $actual = $helper->splitRules($raw);
-
-        $this->assertEquals($expected, $actual);
-
     }
 }
