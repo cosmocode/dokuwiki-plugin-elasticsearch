6f53afc29bcc7a085fcd2cb81150401705f1089e
Andreas Gohr <gohr@cosmocode.de>
2019-07-03 14:15:35 +0200

    add another test for sanity checking our assumptions

--- _test/acl.test.php
+++ _test/acl.test.php
@@ -15,22 +15,33 @@ class helper_plugin_elasticsearch_test extends DokuWikiTest
         '*	@ALL	0',
         'macro:*	@user	0',
         'macro:*	@mino	8',
-        'mini:micro:nano:stop	@ALL	0',
         'mini:*	@user	0',
         'mini:micro:*	mirco	0',
         'mini:micro:*	@user	8',
         'mini:micro:nano:*	@micra	8',
         'mini:micro:nano:*	@nana	8',
-        'mini:micro:nano:stop	spots	8',
         'mini:micro:nano:*	@ALL	0',
+        'mini:micro:nano:stop	spots	8',
+        'mini:micro:nano:stop	@ALL	0',
+        'super:*	@ALL	8',
+        'super:mini:*	@ALL	0',
+        'super:mini:stop	@nana	8',
     ];
 
+    public function dataSanity()
+    {
+        return [
+            ['mini:micro:nano:stop', 'user', 'group', 0],
+            ['mini:micro:nano:stop', 'spots', 'group', 8],
+        ];
+    }
+
     /**
-     * Provide the test data
+     * Provide the test data for the ACL check
      *
      * @return array
      */
-    public function data()
+    public function dataACL()
     {
         return [
             [
@@ -63,13 +74,33 @@ class helper_plugin_elasticsearch_test extends DokuWikiTest
         ];
     }
 
+    /**
+     * Run "normal" ACL checks to make sure we do expect the correct thing to happen
+     *
+     * This is mainly to sanity check the expectations we have for the results in the ACL checks
+     *
+     * @dataProvider dataSanity
+     * @param string $page
+     * @param string $user
+     * @param string $group
+     * @param int $expected
+     */
+    public function testSanity($page, $user, $group, $expected)
+    {
+        global $AUTH_ACL;
+        $AUTH_ACL = $this->acl;
+
+        $this->assertSame($expected, auth_aclcheck($page, $user, [$group]));
+    }
 
     /**
      * Check parsed ACLs for a given page
      *
-     * @dataProvider data
+     * @dataProvider dataACL
+     * @param string $page
+     * @param array $expected
      */
-    public function test_acl($page, $expected)
+    public function testACL($page, $expected)
     {
         /** @var helper_plugin_elasticsearch_acl $helper */
         $helper = plugin_load('helper', 'elasticsearch_acl');
