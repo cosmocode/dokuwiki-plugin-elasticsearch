8656b766807f62e1072663d6551529aae1684cd0
Anna Dabrowska <dabrowska@cosmocode.de>
2022-10-27 14:01:42 +0200

    Correctly display media results

--- action/search.php
+++ action/search.php
@@ -397,7 +397,7 @@ class action_plugin_elasticsearch_search extends DokuWiki_Action_Plugin {
             /** @var Elastica\Result $row */
             $doc = $row->getSource();
             $page = $doc['uri'];
-            if (!page_exists($page) || isHiddenPage($page) || is_file(mediaFN($page)) || auth_quickaclcheck($page) < AUTH_READ) continue;
+            if (!(page_exists($page) || is_file(mediaFN($page))) || isHiddenPage($page) || auth_quickaclcheck($page) < AUTH_READ) continue;
 
             // get highlighted title
             $highlightsTitle = $row->getHighlights()['title'] ?? '';
