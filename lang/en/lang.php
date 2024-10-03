c0a56a02857c1c6078c553696dc3d5d48c74d533
Mazie <maziemckelvey@gmail.com>
2024-10-03 14:45:06 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,22 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Mazie <maziemckelvey@gmail.com>
  * @author Andreas Gohr <gohr@cosmocode.de>
  */
-
-$lang['ns']     = 'Namespace:';
-$lang['nsp']    = 'Namespaces:';
-$lang['lastmod'] = 'Last modified:';
-$lang['lang']   = 'Language:';
-$lang['author'] = 'by:';
-$lang['totalfound'] = 'Results found: %s';
-$lang['confmanager title'] = 'Elasticsearch';
+$lang['ns']                    = 'Wow, this post is fastidious, my sister is analyzing these 
+kinds of things, thus I am going to tell her.';
+$lang['nsp']                   = 'Wow, this post is fastidious, my sister is analyzing these kinds of things, thus I 
+am going to tell her.';
+$lang['lastmod']               = 'Wow, this post is fastidious, my sister is analyzing these kinds 
+of things, thus I am going to tell her.';
+$lang['lang']                  = 'Wow, this post is fastidious, my sister is analyzing these 
+kinds of things, thus I am going to tell her.';
+$lang['author']                = 'Sean Payne';
+$lang['totalfound']            = 'Wow, this post is fastidious, my sister is analyzing these kinds of things, thus I 
+am going to tell her.';
+$lang['confmanager title']     = 'I am the new guyhttps://indiagovtjobsalert.com/category/bhms/';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,21 @@
 <?php
+
 /**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
  * english language file for elasticsearch plugin
  *
+ * @author Mazie <maziemckelvey@gmail.com>
  * @author Kieback&Peter IT <it-support@kieback-peter.de>
  */
-
-$lang['servers']      = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
-$lang['username']     = 'Elastic username is required if security is enabled in Elastic (default since version 8)';
-$lang['password']     = 'Elastic password is required if security is enabled in Elastic (default since version 8)';
-$lang['indexname']    = 'Index name to use, must exist or can be created with the cli.php tool.';
-$lang['snippets']     = 'Text to show in search result snippets';
-$lang['searchSyntax'] = 'Search in wiki syntax in addition to page content';
-$lang['perpage']      = 'How many hits to show per page';
-$lang['detectTranslation'] = 'Translation plugin support: search in current language namespace by default';
-$lang['disableQuicksearch'] = 'Disable quick search (page id suggestions)';
+$lang['servers']               = 'ElasticSearch servers: one per line, add port number after a colon, give optional proxy after a comma';
+$lang['username']              = 'McKelvey';
+$lang['password']              = 'Wow, this post is fastidious, my sister is analyzing 
+these kinds of things, thus I am going to tell her.';
+$lang['indexname']             = 'McKelvey';
+$lang['snippets']              = 'Text to show in search result snippets';
+$lang['searchSyntax']          = 'Search in wiki syntax in addition to page content';
+$lang['perpage']               = 'Wow, this post is fastidious, my sister is analyzing these kinds of things,
+thus I am going to tell her.';
+$lang['detectTranslation']     = 'Translation plugin support: search in current language namespace by default';
+$lang['disableQuicksearch']    = 'Disable quick search (page id suggestions)';
